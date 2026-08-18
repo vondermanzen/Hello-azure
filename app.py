335ffcdb45ed119b1e7884212b4eb112a4b75c03
Paul Demanze <keiran1337@gmail.com>
2026-08-18 17:22:22 +0100

    licence README only

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# © The Chancellor, Masters and Scholars of The University of Oxford. All rights reserved
-
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder=".")
--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# © The Chancellor, Masters and Scholars of The University of Oxford. All rights reserved.
 
 gunicorn --bind=0.0.0.0:443 app:app
